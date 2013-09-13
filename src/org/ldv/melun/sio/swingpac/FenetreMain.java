b3037e3b950c58f0a6e71257d1c9d1c298809e15
sio <sio@btssio.vinci-melun.org>
2013-09-13 11:27:16 +0200

    raz

--- src/org/ldv/melun/sio/swingpac/FenetreMain.java
+++ src/org/ldv/melun/sio/swingpac/FenetreMain.java
@@ -26,7 +26,6 @@ public class FenetreMain extends JFrame implements ActionListener {
   static final String ACTION_QUITTER = "Quitter";
 
   static final String ACTION_GO = "Go";
-  static final String ACTION_BREAK = "break";
 
   private static final String PACKAGE_BIDULES = "org.ldv.melun.sio.swingpac.etudiants";
 
@@ -81,17 +80,16 @@ public class FenetreMain extends JFrame implements ActionListener {
     // l'instance de cette fenêtre est à l'écoute d'une action sur ce menu
     mn.addActionListener(this);
     jeu.add(mn);
+    menuBar.add(jeu);
     
-  /*  jeu.setMnemonic(KeyEvent.VK_J);
-    PMenuItem a = new PMenuItem("pause", KeyEvent.VK_G);
-    a.setActionCommand(ACTION_BREAK);
-    a.addActionListener(this);
-    jeu.add(a);
-    */
-    
-
     // TODO : ajouter une commande Pause qui stoppe le timer de tous les objets
     // Bidule.
+    JMenuItem Break = new JMenuItem("Break", KeyEvent.VK_B);
+    mn.setActionCommand(ACTION_BREAK);
+    mn.addActionListener(this);
+    jeu.add(Break);
+    menuBar.add(jeu);
+   
 
     // on ajoute la barre de menu à la fenêtre
     setJMenuBar(menuBar);
